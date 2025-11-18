3b4f302cd37aaf0a1be784cce6ca0cb2ed928647
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-11-18 01:54:56 -0300

    style: ajustar convenções de nomenclatura em UserModel

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,17 @@
+public class UserModel {
+
+    int idUser;
+
+    String descricao;
+
+    public static final int IDADE_MINIMA = 18;
+
+    public void fetchUsers() {
+    }
+
+    public void getAllUsers() {
+    }
+
+    public void getUsers() {
+    }
+}
