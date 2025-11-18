0298b24d6787c99150c255e69e78f4a0c6fc9a5e
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-11-18 01:51:42 -0300

    refactor: ajustar tratamento de erros conforme Clean Code na classe Error

---
+++ 0x04/error/Error.java
@@ -0,0 +1,22 @@
+public class Error {
+
+    public User createUser(User user) {
+        try {
+            loadUser(user.getId());
+            throw new UserExistsException(user.getId());
+        } catch (UserNotFoundException exception) {
+            // detail here
+        }
+
+        resolveGroup(user);
+        return userRepository.save(user);
+    }
+
+    public void validate(String id) {
+        if (ownerRepo.count(id) == 0) {
+            throw new ValidationException(
+                    "id: " + id + " não foi informado, deveria ser informado..."
+            );
+        }
+    }
+}
